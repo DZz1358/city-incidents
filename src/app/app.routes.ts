2c391edc738402afc0c48200f967152a613833fd
Den <saloed2206@gmail.com>
2025-10-17 16:34:08 +0200

    added some config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'incidents',
+    loadComponent: () =>
+      import('./pages/incidents-list/incidents-list').then(c => c.IncidentsList),
+  },
+  {
+    path: 'incidents/:id',
+    loadComponent: () =>
+      import('./pages/incident-details/incident-details').then(c => c.IncidentDetails),
+  },
+  {
+    path: '',
+    redirectTo: '/incidents',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: '/incidents',
+  },
+];
